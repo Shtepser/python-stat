5273545d2bf13637ddf4b95a96309216b146e99d
Shtepser <38299799+Shtepser@users.noreply.github.com>
2019-01-30 00:12:38 +0300

    Стартовый commit

---
+++ console.py
@@ -0,0 +1,10 @@
+# PYTHON_PATH = "C:/Users/Пользователь/AppData/Local/Programs/Python/Python37-32"
+
+
+def main():
+    print("Main function")
+    exec(open("func.py").read())
+
+
+if __name__ == '__main__':
+    main()
