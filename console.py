24cd6872bdcaa1c76c3b7adef5930dc0fba6e1a2
Shtepser <Shtepser@yandex.ru>
2019-01-30 01:02:46 +0300

    Проверка возможности создания и исполнения скриптов

--- console.py
+++ console.py
@@ -1,13 +1,20 @@
 # PYTHON_PATH = "C:/Users/Пользователь/AppData/Local/Programs/Python/Python37-32"
 
 
-def get_file_type():
-    print("excel file")
+def add_loading(filename):
+    f = open('script.py', 'w')
+    f.write('print("script")')
+
+
+def exec_script():
+    exec(open("script.py").read())
 
 
 def main():
     print("Main function")
-    exec(open("func.py").read())
+    add_loading("file")
+    exec_script()
+
 
 if __name__ == '__main__':
     main()
