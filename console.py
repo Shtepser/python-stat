db166860dc828941b262e221348508e6f864dc4a
Shtepser <38299799+shtepser@users.noreply.github.com>
2019-01-30 00:21:24 +0300

    изменена кодировка readme.txt для корректного чтения

--- console.py
+++ console.py
@@ -1,10 +1,13 @@
 # PYTHON_PATH = "C:/Users/Пользователь/AppData/Local/Programs/Python/Python37-32"
 
 
+def get_file_type():
+    print("excel file")
+
+
 def main():
     print("Main function")
     exec(open("func.py").read())
-
 
 if __name__ == '__main__':
     main()
