e175cb3f3d0222984535000e56778ee67c55e921
shtepser <Shtepser@yandex.ru>
2019-02-02 17:54:06 +0300

    some changes

--- analytic.py
+++ analytic.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 
 class Analytic:
@@ -7,6 +8,16 @@ class Analytic:
         self.source_data = pd.DataFrame()
 
     def load_source_data(self, filepath):
+        file_ext = re.search(r'\.[\w\d]+$', filepath)[0] # расширение файла
+        types = {
+                ".xlsx" : lambda path: pd.read_excel(path),
+                ".xls"  : lambda path: pd.read_excel(path),
+                ".csv"  : lambda path: pd.read_csv(path)
+        }
         # TODO: добавить возможность загрузки .xls и .xlsx файлов, с возможностью автоматического определения
         # TODO: добавить обработку ошибки чтения/парсинга файла
-        self.source_data = pd.read_csv(filepath)
+
+        try:
+            self.source_data = types[file_ext](filepath)
+        except KeyError:
+            raise Exception("Unsupported data type")
--- console.py
+++ console.py
@@ -6,7 +6,7 @@ class ConsoleInterface:
     def __init__(self):
         print("Console interface for python-stat v 0.0.1")
         analyser = analytic.Analytic(self)
-        analyser.load_source_data(input("Enter path to file"))
+        analyser.load_source_data(input("Enter path to file: "))
         print("Checking data:")
         pprint(analyser.source_data)
 
