ada235400555efe959403c5e7bdd39fb31a4e535
Shtepser <Shtepser@yandex.ru>
2019-02-24 03:05:52 +0300

    В процессе - работа с таблицами в GUI Сделано - работа над устранением отображения устаревших данных (если были загружены новые исходные данные, старые должны перестать отображаться)
    TODO: доделать экспорт результатов (в первую очередь - графики)
    TODO: загрузка данных из книг Excel (выбор нужного листа)
    TODO: локализация? (а нужна ли она в консольном режиме?)
    TODO: нормальное venv, чтобы всё это счастье можно было распространять
    
    TODO: графический интерфейс
    
    TODO: два небольших бага консольного интерфейса (описание - в console.py)

--- window.py
+++ window.py
@@ -1,7 +1,5 @@
-from PyQt5 import QtWidgets, QtCore
-
 import analytic
-from PyQt5.QtCore import QDir
+from PyQt5 import QtCore
 from PyQt5.QtWidgets import *
 from StatMainWindow import Ui_MainWindow
 import sys
@@ -9,16 +7,14 @@ import pandas as pd
 
 
 class WindowInterface(QMainWindow, Ui_MainWindow):
-
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.analyser = analytic.Analytic(self)
 
-        # self.tabs_names_index = {}
+        self.tabs_names = {}
 
         self.import_data_action.triggered.connect(self.load_source_data)
-
         self.show()
 
     def show_error(self, message):
@@ -28,26 +24,13 @@ class WindowInterface(QMainWindow, Ui_MainWindow):
         msg.show()
 
     def view_data(self, df: pd.DataFrame, label: str, name: str):
-        qtablewidget = QtWidgets.QTableWidget()
-        headers = df.columns.values.tolist()
-        qtablewidget.setObjectName(name)
-        qtablewidget.setGeometry(QtCore.QRect(5, 10, 741, 491))
-        qtablewidget.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAsNeeded)
-        qtablewidget.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAsNeeded)
-        qtablewidget.setShowGrid(True)
-        qtablewidget.setColumnCount(len(headers))
-        qtablewidget.setHorizontalHeaderLabels(headers)
-
-        for i, row in df.iterrows():
-            # Добавление строки
-            qtablewidget.setRowCount(qtablewidget.rowCount() + 1)
-            for j in range(qtablewidget.columnCount()):
-                qtablewidget.setItem(i, j, QTableWidgetItem(str(row[j])))
-
-        self.dataTabs.addTab(qtablewidget, label)
+        if name in self.tabs_names.keys():
+            self.dataTabs.removeTab(self.dataTabs.indexOf(self.tabs_names[name]))
+        self.tabs_names[name] = self.DataFrameView(df, name)
+        self.dataTabs.addTab(self.tabs_names[name], label)
 
     def load_source_data(self):
-        path = QFileDialog.getOpenFileName(self, "Выберите файл с данными для загрузки", QDir.currentPath(),
+        path = QFileDialog.getOpenFileName(self, "Выберите файл с данными для загрузки", QtCore.QDir.currentPath(),
                                            "Электронные таблицы (*.csv *.xls *.xlsx);;Все файлы (*.*)")[0]
         if path:
             self.analyser.set_filepath(path)
@@ -58,6 +41,34 @@ class WindowInterface(QMainWindow, Ui_MainWindow):
             except FileNotFoundError:
                 self.show_error("File not found")
             self.view_data(self.analyser.source_data, "Исходные данные", "source_data")
+
+    class DataFrameView(QTableView):
+        def __init__(self, data: pd.DataFrame, name: str):
+            QTableView.__init__(self)
+            self.setModel(self.DataFrameModel(data, self))
+            self.setObjectName(name)
+
+        class DataFrameModel(QtCore.QAbstractTableModel):
+            def __init__(self, df: pd.DataFrame, parent=None):
+                QtCore.QAbstractTableModel.__init__(self, parent)
+                self._data = df
+
+            def rowCount(self, parent=None):
+                return self._data.shape[0]
+
+            def columnCount(self, parent=None):
+                return self._data.shape[1]
+
+            def data(self, index, role=QtCore.Qt.DisplayRole):
+                if index.isValid():
+                    if role == QtCore.Qt.DisplayRole:
+                        return str(self._data.iloc[index.row(), index.column()])
+                return None
+
+            def headerData(self, section, orientation, role: int = ...):
+                if orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole:
+                    return self._data.columns[section]
+                return None
 
 
 if __name__ == '__main__':
